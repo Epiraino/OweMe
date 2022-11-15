b98075aa9a0c97a9ae262bc1020f117ccc9857de
Emilio Piraino <piraino91@gmail.com>
2022-11-15 11:08:05 -0800

    Initial commit

---
+++ src/main/java/com/example/oweme/OweMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.oweme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OweMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OweMeApplication.class, args);
+    }
+
+}
